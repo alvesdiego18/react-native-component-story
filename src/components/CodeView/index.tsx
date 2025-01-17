cc80432f5c0d9d76f85b5fabcdf9a0e5dd043cf1
Diego Alves <alvesdiego18@gmail.com>
2025-01-17 18:35:20 -0300

    refactor: created a component code view

---
+++ src/components/CodeView/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {View, Clipboard} from "react-native";
+
+// @ts-ignore
+import SyntaxHighlighter from "react-native-syntax-highlighter";
+
+// @ts-ignore
+import {atomOneDark} from "react-syntax-highlighter/styles/hljs";
+
+import {Button} from "../Button";
+
+interface Props {
+  code: string;
+}
+
+export function CodeView({code}: Props) {
+  const [buttonText, setButtonText] = React.useState("Copy");
+
+  function handleCopy() {
+    Clipboard.setString(code);
+    setButtonText("Copiado");
+
+    const timeout = setTimeout(() => {
+      setButtonText("Copiar");
+      clearTimeout(timeout);
+    }, 800);
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#282c34",
+        justifyContent: "space-between",
+      }}
+    >
+      <View style={{maxHeight: "100%"}}>
+        <SyntaxHighlighter
+          language="jsx"
+          style={atomOneDark}
+          customStyle={{paddingHorizontal: 16, paddingTop: 16}}
+          highlighter="hljs"
+        >
+          {code}
+        </SyntaxHighlighter>
+      </View>
+
+      <View style={{paddingHorizontal: 24, paddingVertical: 16}}>
+        <Button label={buttonText} onPress={handleCopy} blocked />
+      </View>
+    </View>
+  );
+}
