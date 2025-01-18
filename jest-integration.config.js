e49f74c82bd3f57b85edac7a1d71faf47e1a35bf
Diego Alves <alvesdiego18@gmail.com>
2025-01-18 03:35:49 -0300

    refactor: removed unnecessary files

--- jest-integration.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-const config = require("./jest.config");
-
-module.exports = {
-  ...config,
-  testMatch: ["**/?(*.integration.)+(spec|test).[jt]s?(x)"],
-};
--- jest-unit.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-const config = require("./jest.config");
-
-module.exports = {
-  ...config,
-  testMatch: ["**/?(*.unit.)+(spec|test).[jt]s?(x)"],
-};
